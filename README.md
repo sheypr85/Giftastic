23c0ff3cf376f088071c27a7618c9d108f29c1b6
sheylao <sheypr85@gmail.com>
2018-11-08 23:29:00 -0600

    Styling page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Giftastic
+
+Overview:
+
+Used the Giphy API to make a dynamic webpage that let users populate gifs of TV shows or Movies. With the use of Java Script and jQuery the page calls the Giphy API and changes the DOM with the data received. When the gif is displayed, it will be a still image until the user clicks on it and plays it. The User can use the buttons already created on the page or they can create their own by inputing the data.
+
--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var shows = ["Family Guy", "American Horror Story", "Broad City", "Shameless", "Rick and Morty", "Friends", "Game of Thrones", "Homeland", "Big Bang Theory",
-                "American Dad", "Breaking Bad",""]
+                "American Dad", "Breaking Bad", "The Good Place"]
 
         //grab shows and make button from array
         function renderButtons() {
@@ -9,7 +9,7 @@ $(document).ready(function() {
             for (var i = 0; i < shows.length; i++) {
                 var showbutton = $("<button>");
                 showbutton.html(shows[i]);
-                showbutton.addClass("show-button btn btn-light");
+                showbutton.addClass("show-button btn btn-info");
                 showbutton.attr("data-name", shows[i]);
                 $("#buttondiv").append(showbutton);
             }
@@ -20,9 +20,11 @@ $(document).ready(function() {
             var show = $("#show-input").val().trim();
             //prevents button to be created without an input
             if(show != ("")){
-            shows.push(show);
-            renderButtons();
+                shows.push(show);
+                renderButtons();
             }
+            //resets search box
+            $("#show-input").val("");
         });
 
         //get gif from Giphy Api
@@ -30,7 +32,7 @@ $(document).ready(function() {
             $("#gifdiv").empty();
 
             var tvshow = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvshow + "&limit=15&api_key=b1Nn9S5pn3lgtex55XvTgzcDJLLgu4oF";
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvshow + "&limit=10&api_key=b1Nn9S5pn3lgtex55XvTgzcDJLLgu4oF";
 
             $.ajax({
                 url: queryURL,
@@ -42,12 +44,13 @@ $(document).ready(function() {
                 for (var i = 0; i < results.length; i++) {
                     var showgifDiv = $("<div>");
 
+                    //gets the rating from giphy data and puts it in a p tag
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
 
+                    // creates an image from giphy data and gives it an attribute
                     var gifImage = $("<img>");
                     gifImage.attr("src", results[i].images.fixed_height_still.url);
-                    
                     gifImage.attr("data-still", results[i].images.fixed_height_still.url);
                     gifImage.attr("data-animate", results[i].images.fixed_height.url);
                     gifImage.attr("data-state", "still");
@@ -64,12 +67,12 @@ $(document).ready(function() {
         $("#gifdiv").on("click", ".clickimg", function() {
 
             var state = $(this).attr("data-state");
-        
+            //if the gif is still it will animate when clicked
             if(state === "still") {
                 var animate = $(this).attr("data-animate");
                 $(this).attr("src", animate);
                 $(this).attr("data-state", "animate");
-
+            //if the gif is animated it will make it still when clicked
             } else {
                 var still = $(this).attr("data-still");
                 $(this).attr("src", still);
